421c94e624f151594b88e977a533411adc266a7b
Paul Yang <pyang@PYang.local>
2017-11-24 11:43:03 +0800

    test

---
+++ src/main/java/com/example/JenkinsDemoApplication.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsDemoApplication {
+
+        @RequestMapping("/")
+        public String hello()
+        {
+            return "Hello World! This is Jenkins generated!";
+        }
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDemoApplication.class, args);
+	}
+}
